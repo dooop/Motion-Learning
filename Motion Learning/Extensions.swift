2f41244b8087905aa3ad6a199c5398ecca6e9a1d
Dominic Opitz <dominic.opitz@icloud.com>
2016-09-24 03:20:22 +0200

    Remove unused extensions

--- Motion Learning/Extensions.swift
+++ Motion Learning/Extensions.swift
@@ -22,30 +22,3 @@ extension UIView {
         }
     }
 }
-
-extension UIActivityIndicatorView {
-    override func show() {
-        self.startAnimating()
-        super.show()
-    }
-    
-    override func hide() {
-        super.hide()
-        self.stopAnimating()
-    }
-}
-
-extension Array {
-    func splitBy(subSize: Int) -> [[Element]] {
-        return stride(from: 0, to: self.count, by: subSize).map { startIndex in
-            let endIndex = startIndex.advanced(by: subSize)
-            return Array(self[startIndex ..< endIndex])
-        }
-    }
-}
-
-extension String {
-    func insert(string:String, index:Int) -> String {
-        return  String(self.characters.prefix(index)) + string + String(self.characters.suffix(self.characters.count-index))
-    }
-}

9c7354fc85953f9ac337c81e5d71c6fc2b54823a
Dominic Opitz <dominic.opitz@icloud.com>
2016-09-24 03:20:10 +0200

    Implement session view controller with live recording

--- Motion Learning/SessionViewController.swift
+++ Motion Learning/SessionViewController.swift
@@ -11,13 +11,16 @@ import CoreMotion
 
 class SessionViewController: UIViewController {
     
-    @IBOutlet weak var activityIndicator: UIActivityIndicatorView!
-    @IBOutlet weak var pushButton: UIButton!
-    @IBOutlet weak var walkButton: UIButton!
-    @IBOutlet weak var standButton: UIButton!
+    override func viewDidAppear(_ animated: Bool) {
+        MotionRecorder.shared.startRecording(onSequenceRecorded: analyse)
+        
+        Log.shared.write(entry: "Start recording")
+    }
     
-    override func viewDidLoad() {
-        activityIndicator.alpha = 0
+    override func viewWillDisappear(_ animated: Bool) {
+        MotionRecorder.shared.stopRecording()
+        
+        Log.shared.write(entry: "Stop recording")
     }
     
     func analyse(sequence: [CMDeviceMotion]) {
@@ -27,53 +30,5 @@ class SessionViewController: UIViewController {
             
             Log.shared.write(entry: "\(type?.rawValue.uppercased() ?? "NOTHING") detected")
         }
-    }
-    
-    // MARK: - Button actions
-    
-    @IBAction func start(_ sender: UIButton) {
-        activityIndicator.show()
-        
-        MotionRecorder.shared.startRecording(onSequenceRecorded: analyse)
-        
-        Log.shared.write(entry: "Start recording")
-    }
-    
-    @IBAction func stop(_ sender: UIButton) {
-        activityIndicator.hide()
-        
-        MotionRecorder.shared.stopRecording()
-        
-        Log.shared.write(entry: "Stop recording")
-    }
-    
-    @IBAction func loadWeights(_ sender: UIButton) {
-        let success = MotionDetector.shared.resetWeights()
-        
-        Log.shared.write(entry: "Reset weights \(success ? "succeeded" : "failed")")
-    }
-    
-    @IBAction func add(_ sender: UIButton) {
-        let motionType: MotionType?
-        
-        switch sender {
-        case standButton:
-            motionType = .standing
-        case walkButton:
-            motionType = .walking
-        case pushButton:
-            motionType = .pushing
-        default:
-            motionType = nil
-        }
-        
-        guard let sequence = MotionRecorder.shared.sequences.last, let type = motionType else {
-            return
-        }
-        
-        let inputs = MotionDetector.shared.inputs(for: sequence)
-        let totalCalculatedError = MotionDetector.shared.add(inputs: inputs, for: type)
-        
-        Log.shared.write(entry: "Add \(inputs.count) inputs for \(type.rawValue.uppercased()) with total calculated error \(totalCalculatedError)")
     }
 }
